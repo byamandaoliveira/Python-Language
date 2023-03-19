f16005d3156602803ee587cbb7e3837db30bb217
Dev.AmandaOli <114164776+byamandaoliveira@users.noreply.github.com>
2023-03-19 17:55:21 -0300

    Prog.05 - Boolean Type Value
    Manipulation of boolean values.
    ////////////
    
    Manipulação de valores booleanos.
    ////////////

---
+++ Prog.05 - Boolean Type Value.py
@@ -0,0 +1,6 @@
+n1 = bool(input("Type a number: "))
+
+print("The sum is:", n1)
+
+########### COMENTARIOS
+#em bool, ele entende que qualquer coisa digitada como true e quando não digitado nada como false.
