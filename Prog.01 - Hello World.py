a2731a3ec70787c90785fc47d0e03fe3297198b0
Dev.AmandaOli <114164776+byamandaoliveira@users.noreply.github.com>
2023-03-19 17:14:46 -0300

    Prog.01 - Hello World.
    Today March 15, I start my learning about the Python programming language.
    /////////////////
    
    Hoje 15 de março, dou inicio aos meus aprendizados sobre a linguagem de programação Python. 
    /////////////////

---
+++ Prog.01 - Hello World.py
@@ -0,0 +1,5 @@
+print("Hello, World!")
+print('My first code.')
+
+#entre o print e o parentese, não adicione space.
+#na última linha do código, sempre clique em enter para o programa entender que o código acabou ali.
