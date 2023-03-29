9bef93e4914f66a920eca298a83538cafc3d5bf2
Dev.AmandaOli <114164776+byamandaoliveira@users.noreply.github.com>
2023-03-29 16:49:53 -0300

    Prog.07 - Number, the predecessor and sucessor
    Enter a number and see its successor and predecessor.
    
    /////////////////////////////////////////////
    
    Digite um número e veja o sucessor e antecessor dele.

---
+++ Prog.07 - Number, the predecessor and sucessor.py
@@ -0,0 +1,10 @@
+#type a number, the predecessor and sucessor.
+#1option
+number=int(input("Type a number: "))
+pred=number-1
+suce=number+1
+print("The number entered is: {}, the predecessor is {} and sucessor is {}.".format (number, pred, suce))
+
+#2option
+number=int(input("Type a number: "))
+print(f"The number entered is: {number}, the predecessor is {number-1} and sucessor is {number+1}.")
