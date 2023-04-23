9b0f1799f1ef14df4d3ecfdf3d4cf9cd81dff74b
Dev.AmandaOli <114164776+byamandaoliveira@users.noreply.github.com>
2023-04-23 17:07:36 -0300

    Prog.26 - Odd or even
    defining whether the number is even or odd.
    
    /////////////////////////////////////////
    
    definindo se o numero é par ou impar.

---
+++ Prog.26 - Odd or even.py
@@ -0,0 +1,6 @@
+num=int(input("Type a number: "))
+result = num % 2 #qualquer numero DIVIDIDO por 2, numero par dá resultado zero e numero impar, dá um.
+if result == 1:
+    print("This number is ODD!")
+else:
+    print("This number is EVEN!")
